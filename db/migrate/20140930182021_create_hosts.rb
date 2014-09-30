05ccbc3eb95e95a71e83c925194822546cfb3369
annabom <selvaag.braadland@gmail.com>
2014-09-30 21:33:03 +0200

    he

--- db/migrate/20140930182021_create_hosts.rb
+++ db/migrate/20140930182021_create_hosts.rb
@@ -1,8 +1,16 @@
 class CreateHosts < ActiveRecord::Migration
   def change
     create_table :hosts do |t|
-
       t.timestamps
+      #Legges det til id av seg selv?
+      t.firstname :firstname
+      t.lastname :lastname
+      t.address :address
+      t.zipcode :zipcode
+      t.city :city
+      t.phone :phone
+      t.capacity :capacity
+      t.comments :comments
     end
   end
 end
