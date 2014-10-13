b0ccb4b51b28c025e05551a896af00a89eb42ce1
andreawi <andrewis@stud.ntnu.no>
2014-10-13 22:01:05 +0200

    Fixes things after merge

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   root 'hosts#index'
 
-  root 'hosts#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
