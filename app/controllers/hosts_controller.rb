5a4a92119f92549cdf75d9ddd003659fd8bd5018
annabom <selvaag.braadland@gmail.com>
2014-10-22 18:09:41 +0200

    Made show page for applicants

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -30,36 +30,12 @@ class HostsController < ApplicationController
     respond_to do |format|
       if @host.save
         session[:id] = @host.id
-        format.html { redirect_to @host, notice: 'Host was successfully created.' }
+        format.html { redirect_to @host, notice: 'Din soknad er registrert.' }
         format.json { render :show, status: :created, location: @host }
       else
         format.html { render :new }
         format.json { render json: @host.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /hosts/1
-  # PATCH/PUT /hosts/1.json
-  def update
-    respond_to do |format|
-      if @host.update(host_params)
-        format.html { redirect_to @host, notice: 'Host was successfully updated.' }
-        format.json { render :show, status: :ok, location: @host }
-      else
-        format.html { render :edit }
-        format.json { render json: @host.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /hosts/1
-  # DELETE /hosts/1.json
-  def destroy
-    @host.destroy
-    respond_to do |format|
-      format.html { redirect_to hosts_url, notice: 'Host was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -71,6 +47,6 @@ class HostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def host_params
-      params.require(:host).permit(:firstname, :lastname, :address, :zipcode, :city, :phone, :capacity, :comments)
+      params.require(:host).permit(:firstname, :lastname, :address, :zipcode, :city, :phone, :capacity, :comments, :email, :sex, :beenhost)
     end
 end
